a6a12b15de6a193b49848820692a5dd7a8f816e9
ramesh <ramesh@LAPTOP-PU485N3Q>
2023-06-06 09:23:31 +0530

    First Commit

--- CRUDAppSpringCoreThroughAnnotations/src/in/ineuron/dao/EmployeeDaoImpl.java
+++ CRUDAppSpringCoreThroughAnnotations/src/in/ineuron/dao/EmployeeDaoImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import in.ineuron.bo.EmployeeBO;
-import in.ineuron.dto.EmployeeDTO;
 
 @Repository(value = "dao")
 public class EmployeeDaoImpl implements IEmployeeDao {
